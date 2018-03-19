1ae5ff2478f0d541e25fcbd1e8b2f40c5682445e
corrinachow <corrina.yy.chow@gmail.com>
2018-03-18 23:55:58 -0400

    Add grid to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,26 @@ let topOfNav = nav.offsetTop;
 function fixNav() {
   if(window.scrollY >= topOfNav) {
     document.body.style.paddingTop = nav.offsetHeight + 'px';
+    /*nav.style.opacity = 1;*/
     document.body.classList.add('fixed-nav');
   } else {
     document.body.classList.remove('fixed-nav');
+    /*nav.style.opacity = 0;*/
   }
   document.body.style.padding = 0;
 }
 
 function toggleNavBar() {
   nav.classList.toggle('collapse');
-
 }
 
-function resizeSocial() {
+/*function resizeSocial() {
   if (window.innerWidth >= 480) {
     document.getElementById('social-links').style.height = document.getElementById('intro-text').clientHeight + 'px';
     } else if (window.innerWidth < 480) {
     document.getElementById('social-links').style.height = 1.5 + 'rem'
   }
-}
+}*/
 
 window.addEventListener('scroll', fixNav);
 nav.addEventListener('click', toggleNavBar);

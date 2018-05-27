21a3ecbd42e77624aa3bfe5550640a27ba209264
corrinachow <corrina.yy.chow@gmail.com>
2018-05-27 00:21:15 -0400

    Add styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Corrina',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,8 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import './styles.css'
+import './portfolio.css'
 
 const Layout = ({ children, data }) => (
   <div>

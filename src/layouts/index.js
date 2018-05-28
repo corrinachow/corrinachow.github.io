d8bc1b2595322689328d3399a94588e582273145
corrinachow <corrina.yy.chow@gmail.com>
2018-05-27 22:13:54 -0400

    Add navbar and sidebars

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/sidebar'
 import './index.css'
 import './styles.css'
 import './portfolio.css'
@@ -16,15 +17,10 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Navbar siteTitle={data.site.siteMetadata.title} />
+    <Sidebar text="Web development and design portfolio" alignment="sidebar left"/>
+    <Sidebar text={`© ${new Date().getFullYear()}, Corrina Chow`} alignment="sidebar right"/>
+    <div>
       {children()}
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,24 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <header>
+         <div className="title-text">
+            <h1>
+               <span>Corrina Chow</span>
+            </h1>
+            <h2>
+               <span>Web Developer &amp; Designer</span>
+               <p><a aria-label="github" href="https://github.com/corrinachow/"><i aria-hidden className="fab fa-github"></i><span></span></a></p>
+            </h2>
+         </div>
+         <div className="description-text">
+            <h2>
+               <div id="social-links">
+               </div>
+            </h2>
+            <p></p>
+         </div>
+      </header>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )

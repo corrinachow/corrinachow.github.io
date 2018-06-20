f1c389f520af2883b0ca5b0d3ce77dbe23ea8c46
corrinachow <corrina.yy.chow@gmail.com>
2018-06-20 19:07:59 -0400

    Add GraphiQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Corrina Chow | Web Developer and Designer',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }

83cebe93c7700e24de16e915d0ce011d836cb382
corrinachow <corrina.yy.chow@gmail.com>
2018-06-23 00:56:25 -0400

    Add site configs and packages

---
+++ config/SiteConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  siteTitle: 'Corrina Chow | Web Developer and Designer', // Navigation and Site Title
+  siteTitleAlt: 'Corrina Chow - Web Developer and Designer', // Alternative Site title for SEO
+  siteUrl: 'https://corrinachow.com', // Domain of your site. No trailing slash!
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,24 @@
+const config = require('./config/SiteConfig');
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+
+
 module.exports = {
+  pathPrefix: config.pathPrefix,
   siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
     title: 'Corrina Chow | Web Developer and Designer',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
+        name: `projects`,
+        path: `${__dirname}/content/projects`,
       },
     },
   ],

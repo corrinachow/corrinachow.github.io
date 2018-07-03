0985ccdae6e3447f342dd8a6bf54117f3e3110c2
corrinachow <corrina.yy.chow@gmail.com>
2018-07-03 15:22:16 -0400

    Remove portfolio-entry

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from 'gatsby-image'
-import PortfolioEntry from '../components/portfolio-entry'
 
 class Portfolio extends React.Component {
   getList() {

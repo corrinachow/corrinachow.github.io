ccda21e244eb94771b9cb985370e8e518ae5f47b
corrinachow <corrina.yy.chow@gmail.com>
2018-07-03 15:22:04 -0400

    Add navbar toggle on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,51 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const Navbar = ({ siteTitle }) => (
-  <nav className="topnav collapse">
-    <Link to aria-label="open navigation" className="hamburger">
-      &#9776;
-    </Link>
-    <ul className="navigation">
-      <li className="logo">
-        <Link to="#" aria-label="return to home" />
-      </li>
-      <li>
-        <Link to="#about">About</Link>
-      </li>
-      <li>
-        <Link to="#portfolio">Portfolio</Link>
-      </li>
-      <li>
-        <Link to="#contact">Contact</Link>
-      </li>
-      <li className="logo">
-        <Link to aria-label="return to home" href="#" />
-      </li>
-    </ul>
-  </nav>
-)
+class Navbar extends React.Component {
+  constructor(props) {
+    super()
+
+    this.state = {
+      isCollapsed: true,
+    }
+
+    this.toggleNavbar = this.toggleNavbar.bind(this)
+  }
+
+  toggleNavbar() {
+    const { isCollapsed } = this.state
+    this.setState({ isCollapsed: !isCollapsed })
+  }
+
+  render() {
+    return (
+      <nav
+        className={this.state.isCollapsed ? 'topnav collapse' : 'topnav'}
+        onClick={this.toggleNavbar}
+      >
+        <Link to aria-label="open navigation" className="hamburger">
+          &#9776;
+        </Link>
+        <ul className="navigation">
+          <li className="logo">
+            <Link to="#" aria-label="return to home" />
+          </li>
+          <li>
+            <Link to="#about">About</Link>
+          </li>
+          <li>
+            <Link to="#portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="#contact">Contact</Link>
+          </li>
+          <li className="logo">
+            <Link to aria-label="return to home" href="#" />
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
 
 export default Navbar

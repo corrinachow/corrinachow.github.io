6a55e3aeeb93a412926a11a9cdd936d1623a182f
Corrina Chow <corrina.yy.chow@gmail.com>
2020-07-02 13:17:45 -0400

    Feat - Add Twitter SEO

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -23,6 +23,11 @@ const Seo = () => {
       <meta property="og:title" content={siteTitle} />
       <meta property="og:description" content={siteDescription} />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@corrinachow" />
+      <meta
+        name="twitter:image"
+        content="https://images.ctfassets.net/c3b73bxvesf2/69VBcFFCmyu4ufpNoCF8fO/7f035a6ec1cb2aca3f059ea939f696c4/profile-picture.png?h=250"
+      />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:url" content={siteUrl} />
       <meta name="twitter:description" content={siteDescription} />
